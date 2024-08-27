ef3e5e03b833d7baec2e14bdab101dd7a7f7e64e
Pallavinatraj <pallavinatraj31@gmail.com>
2024-08-28 01:15:27 +0530

    Two Sum

---
+++ 1929.js
@@ -0,0 +1,12 @@
+//Concatenation of Array
+function getConcatenation(nums) {
+    let ans = [];
+    for(let i=0; i<=nums.length-1; i++){
+        ans.push(nums[i]);
+    }
+    for(let j=0; j<=nums.length-1; j++){
+        ans.push(nums[j]);
+    }
+    console.log(ans);  
+}
+getConcatenation([1,2,1]);
