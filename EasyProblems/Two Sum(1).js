5be788e45d610755c2f021d91186129193900cea
Pallavi Natraj <143070088+Pallavinatraj@users.noreply.github.com>
2024-08-29 22:19:31 +0530

    Two Sum(1)

--- EasyProblems/Two Sum(1).js
+++ EasyProblems/Two Sum(1).js
@@ -1,4 +1,4 @@
-//Two Sum
+ //Two Sum
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 function twoSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
