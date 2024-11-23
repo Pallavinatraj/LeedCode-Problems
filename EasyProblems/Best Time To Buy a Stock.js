914622fe667fd485685c0934d874cbb1aeec262c
Pallavinatraj <pallavinatraj31@gmail.com>
2024-11-23 23:52:56 +0530

    Problem No.121

---
+++ EasyProblems/Best Time To Buy a Stock.js
@@ -0,0 +1,21 @@
+var maxProfit = function (prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        const profit = prices[i] - minPrice;
+
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
