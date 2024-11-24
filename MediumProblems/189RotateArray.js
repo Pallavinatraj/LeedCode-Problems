1f8ff862552622f4e8dbd6119f1f3bf3784f4511
Pallavinatraj <pallavinatraj31@gmail.com>
2024-11-24 21:01:56 +0530

    Rotate Array 189

---
+++ MediumProblems/189RotateArray.js
@@ -0,0 +1,18 @@
+var rotate = function (nums, k) {
+    if (nums.length === 0) return nums;
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+    return nums;
+};
+
+function reverse(nums, start, end) {
+    while (start < end) {
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
