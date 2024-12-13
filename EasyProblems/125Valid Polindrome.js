d132339e41e5a412e3a54c01a81db5ae5ffa0175
Pallavinatraj <pallavinatraj31@gmail.com>
2024-12-13 10:07:23 +0530

    Problem

---
+++ EasyProblems/125Valid Polindrome.js
@@ -0,0 +1,15 @@
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome
+
+
+
+var isPalindrome = function (s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    let str = s.split('').reverse().join('');
+
+    return str === s;
+
+
+};
---
+++ EasyProblems/680Valid Polindrome.js
@@ -0,0 +1,29 @@
+// Input: s = "abca"
+// Output: true
+// Explanation: You could delete the character 'c'.
+
+
+
+
+var validPalindrome = function (s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPolindrome(s, left + 1, right)
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+function isPolindrome(s, left, right) {
+    while (left < right) {
+        if (s[left] !== s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
