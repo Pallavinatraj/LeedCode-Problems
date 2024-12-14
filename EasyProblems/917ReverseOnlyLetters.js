1c2094f8b4ea0f4a8b1920f2b527cc3580fe8327
Pallavinatraj <pallavinatraj31@gmail.com>
2024-12-14 11:27:35 +0530

    Problem

---
+++ EasyProblems/917ReverseOnlyLetters.js
@@ -0,0 +1,27 @@
+var reverseOnlyLetters = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+    s = s.split("");
+
+    let char = /[a-zA-Z]/g;
+
+    while (left < right) {
+        if (s[left].match(char) === null) {
+            left++;
+            continue;
+        }
+        if (s[right].match(char) === null) {
+            right--;
+            continue;
+        }
+        swap(s, left, right);
+        left++;
+        right--;
+    }
+    return s.join("");
+};
+function swap(s, left, right) {
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+}
